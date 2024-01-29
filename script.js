d5336b7b35c2b196e751a9df9d0bde5c5ea4a469
Alex Burlacu <alexandruburlacu03042000@gmail.com>
2024-01-29 10:32:34 +0100

    prompt per chiedere all'utente i chilometri e l'eta

---
+++ script.js
@@ -0,0 +1,9 @@
+// asking user how many km he wants to travel
+
+let KmToTravel;
+KmToTravel = prompt("Quanti chilometri desideri percorrere?");
+
+// asking user what's his age
+
+let Age;
+Age = prompt("Qual è la tua età?")
